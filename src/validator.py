c3eee110e26f16796509d10ec496cd02bcac7d4e
Abdul Rahem Ariff <aariff1@umd.edu>
2026-04-02 11:38:53 -0400

    Add is_positive helper function

--- src/validator.py
+++ src/validator.py
@@ -39,3 +39,7 @@ def validate_integer(n):
         return num == int(num)
     except (ValueError, TypeError):
         return False
+
+def is_postive(n):
+    """Check if a number is positive."""
+    return n > 0
